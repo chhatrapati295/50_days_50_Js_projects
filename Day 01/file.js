94022665a792aa6d6a40416c8d415d4abb6d0bde
Chhatrapati Chauhan <116446768+chhatrapati295@users.noreply.github.com>
2023-04-06 18:15:38 +0530

    Day 01 completed counter

--- Day 01/file.js
+++ Day 01/file.js
@@ -1,79 +1,34 @@
-const box1 = document.querySelector('.box1')
-const box2 = document.querySelector('.box2')
-const input = document.querySelector('.input')
-const remData = document.querySelector('#one')
-const prevData = document.querySelector('#two')
-const result = document.querySelector('.result')
+const heading = document.querySelector('.heading')
+const decreaseBtn = document.querySelector('.de')
+const resetBtn = document.querySelector('.re')
+const increaseBtn = document.querySelector('.in')
+const allBtns = document.querySelectorAll('button')
+let count = 0;
 
-const startBtn = document.querySelector('.startBtn')
-const checkBtn = document.querySelector('.checkBtn')
-const form = document.querySelector('form')
-let compGuess;
-let remAttempts = 5
-
-startBtn.addEventListener('click',()=>{
-    box1.style.display = 'none';
-    box2.style.display = 'flex';
-    init()
+allBtns.forEach((btn)=>{
+    btn.addEventListener('click',()=>{
+        if(btn.textContent === 'Increase'){
+            count++;
+            numberingColor()
+            heading.textContent = count
+        }else if(btn.textContent === 'Decrease'){
+            count--;
+            numberingColor()
+            heading.textContent = count
+        }else{
+            count =0;
+            numberingColor()
+            heading.textContent = count
+        }
+    })
 })
-function init(){
-    let guessNum = Math.floor(Math.random() *100)
-    compGuess = guessNum
-    remData.textContent = remAttempts;
-    console.log(compGuess)
-        form.addEventListener('submit',(e)=>{
-            e.preventDefault()
-            remAttempts--;
-            if(remAttempts > 0){
-                checkWin()
-            }else{
-                looseFunc()
-            }
-            input.value = ''
-        })
-}
-function checkWin (){
-    if(input.value > 100){
-        result.textContent = 'Number must be 1 to 100'
-        return;
-    }
-    remData.textContent = remAttempts;
-    prevData.textContent += input.value + '  '
-    if(input.value == compGuess){
-        result.textContent = 'You are correct'
-        console.log('right')
-        afterWin()
-    }else if(input.value > compGuess){
-        console.log('Your guess is high')
-        result.textContent = 'Your guess is high'
-    }else{
-        console.log('Your guess is low')
-        result.textContent = 'Your guess is low'
-    }
-}
 
-function afterWin (){
-    form.style.display = 'none'
-    result.style.fontSize = '1.2rem'
-    const btn = document.createElement('button')
-    btn.className = 'restart'
-    btn.textContent = 'Restart'
-    document.querySelector('.attempts').after(btn)
-    btn.addEventListener('click',()=>{
-        window.location.reload()
-    })
-}
-function looseFunc(){
-    form.style.display = 'none'
-    result.style.fontSize = '1.2rem'
-    result.textContent = 'You loose !'
-    const btn = document.createElement('button')
-    btn.className = 'restart'
-    btn.textContent = 'Restart'
-    document.querySelector('.attempts').after(btn)
-    btn.addEventListener('click',()=>{
-        window.location.reload()
-    })
-    remData.textContent = 0;
-    prevData.textContent += input.value + '  '
+function numberingColor (){
+    if(count > 0){
+        heading.style.color = 'green'
+    }else if(count < 0){
+        heading.style.color = 'red'
+    }else{
+        heading.style.color = 'black'
+    }
 }
